f8529bd9993dcf7d8715a82eec08092598e961d2
Isaac Laquerre <60407229+IsaacLaquerre@users.noreply.github.com>
2020-04-21 10:37:33 -0400

    Added more handling to the fishing event

--- FarmingPlugin/src/main/java/net/mistwood/FarmingPlugin/Modules/Fishing/FishingEvents.java
+++ FarmingPlugin/src/main/java/net/mistwood/FarmingPlugin/Modules/Fishing/FishingEvents.java
@@ -4,6 +4,10 @@ import net.mistwood.FarmingPlugin.Main;
 
 import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
+import java.lang.Object
+import org.bukkit.event.Event
+import org.bukkit.event.player.PlayerEvent
+import org.bukkit.event.player.PlayerFishEvent
 
 public class FishingEvents implements Listener
 {
@@ -22,18 +26,29 @@ public class FishingEvents implements Listener
     	//TODO: Handle event
     	Player p = Event.getPlayer();
     	Material item = p.getItemInHand();
-    	if (item.getType() == Material.FISHING_ROD) {
-    		switch(item.getItemMeta().getDisplayName()) {
-    		case "Fishing Rod":
-    			Event.getHook().setBiteChance(0.08);
-    		break;
-    		case "God Rod":
-    			Event.getHook().setBiteChance(1);
-    		break;
-    		default:
-    		break;
-    		}
-    	}
+    	if (Event.State == "FISHING") {
+	    	if (item.getType() == Material.FISHING_ROD) {
+	    		switch(item.getItemMeta().getDisplayName()) {
+	    		case "Fishing Rod":
+	    			Event.getHook().setBiteChance(0.08);
+	    		break;
+	    		case "§bGod Rod":
+	    			Event.getHook().setBiteChance(1);
+	    		break;
+	    		default:
+	    		break;
+	    		}
+	    	}
+    	}else if (Event.State == "BITE") {
+    		p.playSound(p.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1f, 1f);
+    	}else if (Event.State == "CAUGHT_FISH") {
+    		if(Event.getCaught() instanceof Item){
+    			Item caught = (Item) Event.getCaught();
+	    		p.sendMessage("You caught §6%s§r with your %s!", caught.getItemMeta().getDisplayName(), item.getItemMeta().getDisplayName());
+	        }
+	    }else if (Event.State == "FAILED_ATTEMPT") {
+	    	p.playSound(p.getLocation(), Sound.NOTE_BASS, 1f, 1f);
+        }
     }
 
 }
