e07f722dcf0ed5ade72383eac63efa45eb542bfb
IsaacLaquerre <isaac.l@videotron.ca>
2020-04-22 13:57:44 -0400

    Fixed some errors and expanded fishing events handler

--- FarmingPlugin/src/main/java/net/mistwood/FarmingPlugin/Modules/Fishing/FishingEvents.java
+++ FarmingPlugin/src/main/java/net/mistwood/FarmingPlugin/Modules/Fishing/FishingEvents.java
@@ -1,4 +1,4 @@
-  package net.mistwood.FarmingPlugin.Modules.Fishing;
+package net.mistwood.FarmingPlugin.Modules.Fishing;
 
 import net.mistwood.FarmingPlugin.Main;
 
@@ -7,12 +7,15 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.Sound;
 import org.bukkit.event.Event;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerEvent;
 import org.bukkit.event.player.PlayerFishEvent;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Entity;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.ChatColor;
 
 public class FishingEvents implements Listener
 {
@@ -26,39 +29,43 @@ public class FishingEvents implements Listener
         Bukkit.getServer().getPluginManager().registerEvents(this, Instance);
     }
     
-    //@EventHandler
+    @EventHandler
     public void onFish(PlayerFishEvent Event) {
-        //TODO: Handle event
         Player p = Event.getPlayer();
-        p.playSound(p.getLocation(), Sound.BLOCK_NOTE_BLOCK_BELL, 1f, 1f);
-        p.sendMessage("Fish rod casted");
-        int xpToDrop = 0;
-        ItemStack item = p.getInventory().getItemInMainHand();
+        //p.playSound(p.getLocation(), Sound.BLOCK_NOTE_BLOCK_BELL, 1f, 1f);
+        int xpToDrop = 50;
+        ItemStack rod = p.getInventory().getItemInMainHand();
+        String rodName = rod.getItemMeta().hasDisplayName() ? rod.getItemMeta().getDisplayName() : capitalize(rod.getType().name().replace("_", " ").toLowerCase());
         Event.setExpToDrop(xpToDrop);
-        if (Event.getState().toString() == "FISHING") {
-            if (item.getType() == Material.FISHING_ROD) {
-                switch(item.getItemMeta().getDisplayName()) {
-                case "Fishing Rod":
-                    Event.getHook().setBiteChance(0.08);
-                break;
-                case "§bGod Rod":
-                    Event.getHook().setBiteChance(1);
-                break;
-                default:
-                break;
+        if (Event.getState() == PlayerFishEvent.State.FISHING) {
+            p.sendMessage(rodName + ChatColor.RESET + " casted");
+            if (rod.getType() == Material.FISHING_ROD) {
+                if (rodName == "Fishing Rod") {
+                    //TODO: stuff
+                }else if (rodName == ChatColor.AQUA + "God Rod") {
+                    //TODO: more stuff
                 }
             }
-        }else if (Event.getState().toString() == "BITE") {
+        }else if (Event.getState() == PlayerFishEvent.State.BITE) {
             //TODO: Add event when fish is on hook
-        }else if (Event.getState().toString() == "CAUGHT_FISH") {
-            if(Event.getCaught() instanceof ItemStack){
-                ItemStack caught = (ItemStack) Event.getCaught();
-                p.playSound(p.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1f, 1f);
-                p.sendMessage("You caught §6" + caught.getItemMeta().getDisplayName() + "§r with your " + item.getItemMeta().getDisplayName() + "!");
-            }
-        }else if (Event.getState().toString() == "FAILED_ATTEMPT") {
+        }else if (Event.getState() == PlayerFishEvent.State.CAUGHT_ENTITY || Event.getState() == PlayerFishEvent.State.CAUGHT_FISH) {
+        	if(Event.getCaught() == null) return;
+            Entity caught = (Entity) Event.getCaught();
+            p.playSound(p.getLocation(), Sound.BLOCK_NOTE_BLOCK_BELL, 1f, 1f);
+            p.sendMessage("You caught a " + ChatColor.GOLD + caught.getName() + ChatColor.RESET + " with your " + rodName + "!");
+        }else if (Event.getState() == PlayerFishEvent.State.FAILED_ATTEMPT) {
             p.playSound(p.getLocation(), Sound.BLOCK_NOTE_BLOCK_BASS, 1f, 1f);
         }
     }
 
+	private String capitalize(String str) {
+	    String words[] = str.split("\\s");  
+	    String capitalizeWord = "";  
+	    for(String w:words){  
+	        String first = w.substring(0,1);  
+	        String afterfirst = w.substring(1);  
+	        capitalizeWord += first.toUpperCase()+afterfirst+" ";  
+	    }  
+	    return capitalizeWord.trim();  
+	}
 }
