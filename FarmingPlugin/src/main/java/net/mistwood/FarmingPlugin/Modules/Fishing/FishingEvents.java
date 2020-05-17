62c0c4b944a88bb03012bd5526448552e305cd73
Daniel Munch <daniellmunch@gmail.com>
2020-05-17 16:55:58 +0200

    Refactored fishing event code

--- FarmingPlugin/src/main/java/net/mistwood/FarmingPlugin/Modules/Fishing/FishingEvents.java
+++ FarmingPlugin/src/main/java/net/mistwood/FarmingPlugin/Modules/Fishing/FishingEvents.java
@@ -16,11 +16,13 @@ import net.mistwood.FarmingPlugin.FarmingPlugin;
 import net.mistwood.FarmingPlugin.Utils.Messages;
 import net.mistwood.FarmingPlugin.Utils.Helper;
 
-import java.util.Random;
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
+
+import static java.util.Arrays.asList;
 
 public class FishingEvents implements Listener {
+
+    private final static List<Material> replacements = asList(Material.COD, Material.SALMON, Material.TRIPWIRE_HOOK, Material.LILY_PAD, Material.NAME_TAG, Material.PUFFERFISH);
 
     public FishingEvents() {
         Bukkit.getServer().getPluginManager().registerEvents(this, FarmingPlugin.instance);
@@ -35,72 +37,77 @@ public class FishingEvents implements Listener {
         String rodName = rod.getItemMeta().hasDisplayName() ? rod.getItemMeta().getDisplayName() : Helper.capitalize(rod.getType().name().replace("_", " ").toLowerCase());
         event.setExpToDrop(xpToDrop);
 
-        if (event.getState() == PlayerFishEvent.State.FISHING) {
-            // ...
-        } else if (event.getState() == PlayerFishEvent.State.BITE) {
-            playSound(player, player.getLocation(), Sound.BLOCK_NOTE_BLOCK_BELL, 0.5f, 1f);
-            Helper.sendMessage(player, Messages.FISHING_BITE);
-        } else if (event.getState() == PlayerFishEvent.State.CAUGHT_ENTITY || event.getState() == PlayerFishEvent.State.CAUGHT_FISH) {
-            Item caught = (Item) event.getCaught();
-            List<Item> fishDrops = new ArrayList<>();
-            if (caught == null) return;
+        Item caughtItem = (Item) event.getCaught();
+        List<Item> fishDrops = new ArrayList<>();
 
-            Material[] replacements = {Material.COD, Material.SALMON, Material.TRIPWIRE_HOOK, Material.LILY_PAD, Material.NAME_TAG, Material.PUFFERFISH};
+        switch (event.getState()) {
+            case FISHING: break;
 
-            if (rod.getItemMeta().hasLore()) {
-                if (rod.getItemMeta().getLore().size() > 1) {
-                    List<String> modifiers = rod.getItemMeta().getLore().subList(2, rod.getItemMeta().getLore().size());
-                    for (int i = 0; i < modifiers.size(); i++) {
-                        String[] modifiersArray = new String[modifiers.size()];
-                        modifiersArray = modifiers.toArray(modifiersArray);
-                        if (modifiersArray[i].startsWith("LOOT: ")) {
-                            int lootmultiplier = Integer.parseInt(modifiersArray[i].substring(6, 7));
-                            caught.getItemStack().setAmount(lootmultiplier);
-                        } else if (modifiersArray[i].startsWith("EXTRA LOOT ")) {
-                            int extraLootLvl = Integer.parseInt(modifiersArray[i].substring(11, 12));
-                            for (int ind = 0; i < extraLootLvl; i++) {
-                                int random = new Random().nextInt(replacements.length);
-                                ItemStack extraItem = new ItemStack(replacements[random]);
-                                extraItem.setAmount(caught.getItemStack().getAmount());
-                                Item droppedItem = caught.getWorld().dropItem(caught.getLocation(), extraItem);
-                                Vector velocity = (player.getLocation().toVector().subtract(event.getHook().getLocation().toVector())).multiply(0.1);
-                                droppedItem.setVelocity(velocity.setY(velocity.getY() + 0.2));
-                                fishDrops.add(droppedItem);
+            case BITE: {
+                playSound(player, player.getLocation(), Sound.BLOCK_NOTE_BLOCK_BELL, 0.5f, 1f);
+                Helper.sendMessage(player, Messages.FISHING_BITE);
+                break;
+            }
+
+            case CAUGHT_FISH:
+            case CAUGHT_ENTITY: {
+                if (caughtItem == null) return;
+
+                List<String> lore = rod.getItemMeta().getLore();
+                if (lore != null) {
+                    List<String> modifiers = lore.subList(2, lore.size());
+                    for (String modifier : modifiers) {
+                        switch (modifier.trim()) {
+                            case "LOOT:": {
+                                int amountMultiplier = Integer.parseInt(modifier.substring(6, 7));
+                                caughtItem.getItemStack().setAmount(amountMultiplier);
+                            }
+
+                            case "EXTRA LOOT": {
+                                int extraLootLvl = Integer.parseInt(modifier.substring(11, 12));
+                                for (int i = 0; i < extraLootLvl; i++) {
+                                    int random = new Random().nextInt(replacements.size());
+                                    ItemStack extraItem = new ItemStack(replacements.get(random), caughtItem.getItemStack().getAmount());
+
+                                    Item droppedItem = caughtItem.getWorld().dropItem(caughtItem.getLocation(), extraItem);
+                                    Vector velocity = (player.getLocation().toVector().subtract(event.getHook().getLocation().toVector())).multiply(0.1);
+                                    droppedItem.setVelocity(velocity.setY(velocity.getY() + 0.2));
+                                    fishDrops.add(droppedItem);
+                                }
                             }
                         }
                     }
                 }
-            }
-            fishDrops.add(caught);
 
-            playSound(player, player.getLocation(), Sound.BLOCK_NOTE_BLOCK_CHIME, 0.5f, 1f);
-            playSound(player, player.getLocation(), Sound.BLOCK_NOTE_BLOCK_CHIME, 0.5f, 3f);
-            playSound(player, player.getLocation(), Sound.BLOCK_NOTE_BLOCK_CHIME, 0.5f, 5f);
+                fishDrops.add(caughtItem);
+                playSound(player, player.getLocation(), Sound.BLOCK_NOTE_BLOCK_CHIME, 0.5f, 1f);
+                playSound(player, player.getLocation(), Sound.BLOCK_NOTE_BLOCK_CHIME, 0.5f, 3f);
+                playSound(player, player.getLocation(), Sound.BLOCK_NOTE_BLOCK_CHIME, 0.5f, 5f);
 
-            List<String> finalLoot = new ArrayList<>();
-
-            for (int o = 0; o < fishDrops.size(); o++) {
-                int oldAmount = fishDrops.get(o).getItemStack().getAmount();
-                for (int c = o + 1; c < fishDrops.size(); c++) {
-                    if (c != 0 && fishDrops.get(o).getItemStack().getType() == fishDrops.get(c).getItemStack().getType()) {
-                        fishDrops.get(o).getItemStack().setAmount(fishDrops.get(o).getItemStack().getAmount() + fishDrops.get(c).getItemStack().getAmount());
-                        fishDrops.remove(c);
+                Map<Material, Integer> loot = new HashMap<>();
+                for (Item item : fishDrops) {
+                    ItemStack stack = item.getItemStack();
+                    int amount = stack.getAmount();
+                    if (loot.containsKey(stack.getType())) {
+                        amount += loot.get(stack.getType());
                     }
+                    loot.put(stack.getType(), amount);
                 }
-                String plural = "";
-                if (fishDrops.get(o).getItemStack().getAmount() != 1) {
-                    if (fishDrops.get(o).getItemStack().getType().toString().toLowerCase().endsWith("h")) {
-                        plural = "es";
-                    } else plural = "s";
-                }
-                finalLoot.add(fishDrops.get(o).getItemStack().getAmount() + " " + fishDrops.get(o).getName() + plural);
-                fishDrops.get(o).getItemStack().setAmount(oldAmount);
+
+                List<String> dropNames = new ArrayList<>();
+                loot.forEach((item, amount) -> {
+                    // TODO: Does this return the correct itemstack name?
+                    dropNames.add(amount + " " + new ItemStack(item).getItemMeta().getDisplayName());
+                });
+
+                Helper.sendMessage(player, String.format(Messages.FISHING_CAUGHT_ENTITY, String.join(", ", dropNames).replaceFirst(",(?=[^,]+$)", " and"), rodName));
             }
 
-            Helper.sendMessage(player, String.format(Messages.FISHING_CAUGHT_ENTITY, String.join(", ", finalLoot).replaceFirst(",(?=[^,]+$)", " and"), rodName));
-        } else if (event.getState() == PlayerFishEvent.State.FAILED_ATTEMPT) {
-            playSound(player, player.getLocation(), Sound.BLOCK_NOTE_BLOCK_BASS, 1f, 1f);
-            Helper.sendMessage(player, Messages.FISHING_BITE_FAIL);
+            case FAILED_ATTEMPT: {
+                playSound(player, player.getLocation(), Sound.BLOCK_NOTE_BLOCK_BASS, 1f, 1f);
+                Helper.sendMessage(player, Messages.FISHING_BITE_FAIL);
+                break;
+            }
         }
     }
 
