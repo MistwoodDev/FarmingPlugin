6ae14870e69f3990b2a75d2f217eed556d4f281f
Daniel Munch <daniellmunch@gmail.com>
2020-05-18 13:42:14 +0200

    Refactored fishing module file

--- FarmingPlugin/src/main/java/net/mistwood/FarmingPlugin/Modules/Fishing/DisableEnchanting.java
+++ FarmingPlugin/src/main/java/net/mistwood/FarmingPlugin/Modules/Fishing/DisableEnchanting.java
@@ -1,29 +1,21 @@
 package net.mistwood.FarmingPlugin.Modules.Fishing;
 
-import org.bukkit.event.enchantment.EnchantItemEvent;
-import net.mistwood.FarmingPlugin.FarmingPlugin;
-
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
 
+import org.bukkit.event.enchantment.EnchantItemEvent;
+
 public class DisableEnchanting implements Listener {
 
-    private static FarmingPlugin Instance;
-
-    public DisableEnchanting (FarmingPlugin Instance)
-    {
-        DisableEnchanting.Instance = Instance;
-    }
-    
     @EventHandler
-    public void onEnchant (EnchantItemEvent Event) {
-    	ItemStack Rod = Event.getItem ();
-    	
-        if (Rod.getItemMeta ().hasLore ()) {
-        	if (Rod.getItemMeta ().getLore ().get (0).startsWith ("Fishing rod - ")) {
-            	Event.setCancelled (true);
-        	}
+    public void onEnchant(EnchantItemEvent event) {
+        ItemStack rod = event.getItem();
+
+        if (rod.getItemMeta().hasLore()) {
+            if (rod.getItemMeta().getLore().get(0).startsWith("Fishing rod - ")) {
+                event.setCancelled(true);
+            }
         }
     }
 }
