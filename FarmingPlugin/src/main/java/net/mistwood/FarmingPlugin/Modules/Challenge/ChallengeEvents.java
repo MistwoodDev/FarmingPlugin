af7411401b1c5ae84fd4192200bc110c37e962f9
Daniel Munch <daniellmunch@gmail.com>
2020-08-30 20:08:26 +0200

    Changed the way challenge events is handled and experiment with new way of getting farm data for it

--- FarmingPlugin/src/main/java/net/mistwood/FarmingPlugin/Modules/Challenge/ChallengeEvents.java
+++ FarmingPlugin/src/main/java/net/mistwood/FarmingPlugin/Modules/Challenge/ChallengeEvents.java
@@ -11,25 +11,18 @@ import net.mistwood.FarmingPlugin.FarmingPlugin;
 public class ChallengeEvents implements Listener
 {
 
-    private final ChallengeManager manager;
-
     public ChallengeEvents (ChallengeManager manager)
     {
-        this.manager = manager;
-
         for (Class event : manager.getEvents())
         {
-            Bukkit.getPluginManager ().registerEvent (event, new Listener () {}, EventPriority.NORMAL, (listener, e) -> {
-                manager.emit(e.getClass (), e, null); // TODO: Somehow find farm data here
+            Bukkit.getPluginManager().registerEvent(event, new Listener () {}, EventPriority.NORMAL, (listener, e) -> {
+                manager.emit(
+                    e.getClass (),
+                    e,
+                    FarmingPlugin.instance.farmsCache.get(FarmingPlugin.instance.playersCache.get(event.getPlayer().getUniqueId()).farmID)
+                ); // TODO: Test this method of getting farm data
             }, FarmingPlugin.instance);
         }
-    }
-
-    @EventHandler
-    public void onPlayerAdvancementDone (PlayerAdvancementDoneEvent event)
-    {
-        // assuming the target player is in a farm, which we would normally check for
-        manager.emit(event.getClass (), event, FarmingPlugin.instance.farmsCache.get(FarmingPlugin.instance.playersCache.get(event.getPlayer ().getUniqueId ()).farmID));
     }
 
 }
